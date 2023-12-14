177ad64c82323f273645dda98ea4ef6d0c95ca93
tomandjerry17 <144357285+tomandjerry17@users.noreply.github.com>
2023-12-14 04:30:45 -0800

    Add files via upload

--- Martinez_Function_Activity.py
+++ Martinez_Function_Activity.py
@@ -1,25 +1,27 @@
 def threeN(x, y, z):
-    try:
-        if x == y == z:
-            print("\nResult is ", int(x) * int(y) * int(z))
-        elif x == y:
-            print("\nResult is ", int(x) * int(y) + int(z))
-        elif x == z:
-            print("\nResult is ", int(x) * int(z) + int(y))
-        elif z == y:
-            print("\nResult is ", int(x) + int(y) * int(z))
-        else:
-            print("\nResult is ", int(x) + int(y) + int(z))
-    except ValueError:
-        print("Enter a Valid Integer!")
+    if x == y == z:
+        print("\nResult is ", int(x) * int(y) * int(z))
+    elif x == y:
+        print("\nResult is ", int(x) * int(y) + int(z))
+    elif x == z:
+        print("\nResult is ", int(x) * int(z) + int(y))
+    elif z == y:
+        print("\nResult is ", int(x) + int(y) * int(z))
+    else:
+        print("\nResult is ", int(x) + int(y) + int(z))
 
 
 def main():
-
-    first = int(input("Enter First Number  : "))
-    second = int(input("Enter Second Number : "))
-    third = int(input("Enter Third Number  : "))
-    threeN(first, second, third)
+    while True:
+        try:
+            print()
+            first = int(input("Enter First Number  : "))
+            second = int(input("Enter Second Number : "))
+            third = int(input("Enter Third Number  : "))
+            threeN(first, second, third)
+        except ValueError:
+            print("Enter a Valid Integer!\n\n")
+            continue
 
 
 main()
